945bf18c00cc5d810179e1652cfe2a48b5c878a2
Alexander Kalmykov <Alexander.Kalmykov@logianalytics.com>
2023-04-06 14:01:46 +0200

    added google and fb analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,44 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <>
-    <Head>
-      <title>
-        Radius real estate Київ
-      </title>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"/>
-    </Head>
-    <Component {...pageProps} />
-  </>;
+	return <>
+		<Head>
+			<!-- Google tag (gtag.js) -->
+			<script async src="https://www.googletagmanager.com/gtag/js?id=G-FRNJCTGK53"></script>
+			<script>
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+
+				gtag('config', 'G-FRNJCTGK53');
+			</script>
+
+			<!-- Meta Pixel Code -->
+			<script>
+				!function(f,b,e,v,n,t,s)
+				{if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+					n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+				if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+				n.queue=[];t=b.createElement(e);t.async=!0;
+				t.src=v;s=b.getElementsByTagName(e)[0];
+  				s.parentNode.insertBefore(t,s)}(window, document,'script',
+				'https://connect.facebook.net/en_US/fbevents.js');
+				fbq('init', '1076107899983407');
+				fbq('track', 'PageView');
+			</script>
+			<noscript><img height="1" width="1" style="display:none"
+				src="https://www.facebook.com/tr?id=1076107899983407&ev=PageView&noscript=1"
+			/></noscript>
+			<!-- End Meta Pixel Code -->
+			
+			<title>
+				Radius real estate Київ
+			</title>
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+		</Head>
+		<Component {...pageProps} />
+	</>;
 }
 
