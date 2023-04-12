b1a413a6e1c86a87d761ef360e331048e0d0098a
Alexander Kalmykov <Alexander.Kalmykov@logianalytics.com>
2023-04-12 21:28:42 +0200

    research

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
 			<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-			<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/> 
+			<meta http-equiv="Content-Security-Policy" content="accept-insecure-requests"/> 
 		</Head>
 		<Component {...pageProps} />
 	</>;
